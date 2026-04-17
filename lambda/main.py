0ef14cccdf03714e56a4038f908c3fcea8ac68d8
GeoffA12 <geoffarroyo@gmail.com>
2026-04-16 20:41:41 -0500

    Add dev deploy action. Update lambda handler

--- lambda/main.py
+++ lambda/main.py
@@ -2,7 +2,11 @@ import json
 
 
 def handler(event, context):
+    response_body = {
+        "message": "Hello World",
+        "version": "1.0.0"
+    }
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello World")
+        "body": json.dumps(response_body)
     }
